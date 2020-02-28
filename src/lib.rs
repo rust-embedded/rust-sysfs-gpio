dff43d8bf6769427ccc467067257240e5d425bb3
Aphek <bilkow@tutanota.com>
2020-02-28 02:03:18 -0300

    Add missing set_direction in main example.

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@
 //! fn main() {
 //!     let my_led = Pin::new(127); // number depends on chip, etc.
 //!     my_led.with_exported(|| {
+//!         my_led.set_direction(Direction::Out).unwrap();
 //!         loop {
 //!             my_led.set_value(0).unwrap();
 //!             sleep(Duration::from_millis(200));
